9198c2368a3bd21d879849472451cd675affa0e7
Raryson Pereira Rost <raryson.rost@gmail.com>
2016-12-29 11:08:41 -0200

    count funcionando

--- routes.js
+++ routes.js
@@ -4,10 +4,11 @@ var MongoClient = require('mongodb').MongoClient;
    var mongo = require('./mongo.js');
 
   app.get('/',function(req, res){
-      var nome = '', idade = '', listao = [];
+      var nome = '', idade = '', listao = [], numero = '';
       res.render('index',{
         nome: nome,
         idade: idade,
+        numero: numero,
         listao: listao
       })
   })
@@ -15,10 +16,16 @@ var MongoClient = require('mongodb').MongoClient;
   app.post('/',function(req, res){
     var name = req.body.nome;
     var idade = req.body.idade;
-    var pessoa = { nome:name, idade: idade};
     MongoClient.connect("mongodb://localhost:27017/teste", function(err, db) {
       if(!err) {
           var collection = db.collection('Pessoas');
+          collection.find().count().then(function(numItems) {
+            var numero = numItems+1;
+            console.log(numero);
+            callback(numItems);
+          })
+          var pessoa = { nome:name, idade: idade, numero: numero};
+          console.log(pessoa)
           collection.insert(pessoa, {w:1}, function(err, result) {})
           const resultadoDoFind = collection.find();
           resultadoDoFind.toArray(function(err, items) {
@@ -34,9 +41,12 @@ var MongoClient = require('mongodb').MongoClient;
 
 
   app.post('/remover',function(req, res){
-    MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+    MongoClient.connect("mongodb://localhost:27017/teste", function(err, db) {
       if(!err) {
-          var collection = db.collection('CS_Umbler');
+        console.log("maoe")
+          var collection = db.collection('Pessoas');
+
+
           collection.deleteOne({a:1}, function(err, items) {
             if (err) {
               res.send(err);
