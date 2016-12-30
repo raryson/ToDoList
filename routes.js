007b269544ea793a577cb67b22fdefa06f43c788
Raryson Pereira Rost <raryson.rost@gmail.com>
2016-12-30 15:43:31 -0200

    Vai heroku

--- routes.js
+++ routes.js
@@ -2,24 +2,22 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require("mongodb").ObjectID;
 
  module.exports = function(app) {
-   var mongo = require('./mongo.js');
+
 
    app.get('/', (req, res) => {
-     res.send('olar')
+     MongoClient.connect("mongodb://raryson:123456@ds149258.mlab.com:49258/teste", function(err, db) {
+       if(!err) {
+         var collection = db.collection('Pessoas3')
+         const resultadoDoFind = collection.find();
+         resultadoDoFind.toArray(function(err, items) {
+         listao = items;
+         res.render('index')
+   });
+   db.close();
+ }});
    })
 
-  app.get('/listao',function(req, res){
-      MongoClient.connect("mongodb://raryson:123456@ds149258.mlab.com:49258/teste", function(err, db) {
-        if(!err) {
-          var collection = db.collection('Pessoas3')
-          const resultadoDoFind = collection.find();
-          resultadoDoFind.toArray(function(err, items) {
-          listao = items;
-          res.render('index')
-    });
-    db.close();
-  }});
-});
+  
   app.post('/adicionar',function(req, res){
     var name = req.body.nome;
     var idade = req.body.idade;
