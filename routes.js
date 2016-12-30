db0a650c59555c02fe42dd170ef8c65baa2a90bc
Raryson Pereira Rost <raryson.rost@gmail.com>
2016-12-30 13:06:37 -0200

    Alterei para o MLAB

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ var ObjectID = require("mongodb").ObjectID;
    var mongo = require('./mongo.js');
 
   app.get('/',function(req, res){
-      MongoClient.connect("mongodb://localhost:27017/teste", function(err, db) {
+      MongoClient.connect("mongodb://raryson:123456@ds149258.mlab.com:49258/teste", function(err, db) {
         if(!err) {
           var collection = db.collection('Pessoas3')
           const resultadoDoFind = collection.find();
@@ -19,7 +19,7 @@ var ObjectID = require("mongodb").ObjectID;
   app.post('/adicionar',function(req, res){
     var name = req.body.nome;
     var idade = req.body.idade;
-    MongoClient.connect("mongodb://localhost:27017/teste", function(err, db) {
+    MongoClient.connect("mongodb://raryson:123456@ds149258.mlab.com:49258/teste", function(err, db) {
       if(!err) {
           var collection = db.collection('Pessoas3')
           var pessoa = { nome:name, idade: idade};
@@ -35,7 +35,7 @@ var ObjectID = require("mongodb").ObjectID;
 
 
   app.post('/remover',function(req, res){
-    MongoClient.connect("mongodb://localhost:27017/teste", function(err, db) {
+    MongoClient.connect("mongodb://raryson:123456@ds149258.mlab.com:49258/teste", function(err, db) {
       if(!err) {
           var collection = db.collection('Pessoas3');
           var id = req.body.id
