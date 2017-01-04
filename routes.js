b58a693d66035be0cf7e23fd3ba828d057acb7f9
Raryson Pereira Rost <raryson.rost@gmail.com>
2017-01-04 21:09:55 -0200

    Configurado o sistema de login, alteração na estrutura do banco

--- routes.js
+++ routes.js
@@ -7,17 +7,25 @@ var ObjectID = require("mongodb").ObjectID;
      res.render('home/index')
    })
 
-   app.get('/', (req, res) => {
+   app.get('/lista', (req, res) => {
+     if(req.cookies.email === undefined){
+       res.render('error')
+     }else{
      MongoClient.connect("mongodb://raryson:123456@ds149258.mlab.com:49258/teste", function(err, db) {
        if(!err) {
-         var collection = db.collection('Pessoas3')
-         const resultadoDoFind = collection.find();
+         var collection = db.collection(req.cookies.email)
+         const resultadoDoFind = collection.find().skip(1);
          resultadoDoFind.toArray(function(err, items) {
          listao = items;
-         res.render('index')
+         res.render('lista/index')
    });
    db.close();
  }});
+ }
+   })
+
+   app.get('/',(req, res) => {
+     res.render('home/index')
    })
 
 
@@ -27,6 +35,8 @@ var ObjectID = require("mongodb").ObjectID;
         var collection = db.collection(req.body.email);
             objetoElementoASerCadastrado = {email: req.body.email, senha: req.body.senha, list: [{}] }
         collection.insert(objetoElementoASerCadastrado, function(err, result) {})
+            primeiroRegistro = {elementoAserCadastrado:"Este é o teu primeiro registro :)"}
+        collection.insert(primeiroRegistro, function(err, result) {})
       }db.close()
     })
       res.sendStatus(200);
@@ -38,10 +48,10 @@ var ObjectID = require("mongodb").ObjectID;
         var collection = db.collection(req.body.email);
         const resultadoDoFind = collection.find({email:req.body.email ,senha: req.body.senha});
         resultadoDoFind.toArray(function(err, items) {
-        if(items.length == 1){
+        if(items.length === 1){
           res.json({logou:"Logado com sucesso",email:req.body.email})
         }else{
-          res.send("senha incorreta ou usuário inválido")
+          res.sendStatus(500);
         }
           });
         }
@@ -54,7 +64,7 @@ var ObjectID = require("mongodb").ObjectID;
   app.post('/adicionar',function(req, res){
     MongoClient.connect("mongodb://raryson:123456@ds149258.mlab.com:49258/teste", function(err, db) {
       if(!err) {
-          var collection = db.collection('Users')
+          var collection = db.collection(req.cookies.email)
           var objetoElementoASerCadastrado = { elementoAserCadastrado:req.body.elementoAserCadastrado};
           collection.insert(objetoElementoASerCadastrado, function(err, result) {
             var idDoObjetoDoMongo = result.insertedIds.toString()
@@ -70,7 +80,7 @@ var ObjectID = require("mongodb").ObjectID;
   app.post('/remover',function(req, res){
     MongoClient.connect("mongodb://raryson:123456@ds149258.mlab.com:49258/teste", function(err, db) {
       if(!err) {
-          var collection = db.collection('Pessoas3');
+          var collection = db.collection(req.cookies.email);
           var id = req.body.id
           collection.deleteOne({_id: ObjectID(id)}, function(err, items) {
             if (err) {
